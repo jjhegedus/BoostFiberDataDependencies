fe969dd073677c7ddf94a8b2a57909012acdd2b8
Jeff Hegedus <jeff@22ndtech.com>
2020-03-22 20:01:25 -0600

    Added MultiItemStore

--- BoostFiberDataDependencies.cpp
+++ BoostFiberDataDependencies.cpp
@@ -1,3 +1,4 @@
+
 // BoostFiberDataDependencies.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
--- FiberedQueueProcessor.h
+++ FiberedQueueProcessor.h
@@ -3,6 +3,8 @@
 #include "ThreadSafeQueue.h"
 #include "NamedItemStore.h"
 #include <boost/fiber/all.hpp>
+#include "MultiItemStore.h"
+#include <iostream>
 
 namespace ndtech {
 
@@ -12,6 +14,7 @@ namespace ndtech {
     ndtech::ThreadSafeQueue<std::string>* m_q;
     bool m_running = true;
     NamedItemStore<std::string> m_store;
+    MultiItemStore<std::string, std::string, int> m_multiItemStore;
 
     FiberedQueueProcessor(ndtech::ThreadSafeQueue<std::string>* q) {
       m_q = q;
@@ -29,6 +32,8 @@ namespace ndtech {
     void Run() {
       boost::fibers::fiber itemStoreFiber;
       m_store.RunOn(itemStoreFiber);
+      boost::fibers::fiber multiStoreFiber;
+      m_multiItemStore.RunOn(multiStoreFiber);
 
       while (m_running) {
         ItemType item = m_q->Pop();
@@ -38,74 +43,127 @@ namespace ndtech {
           m_running = false;
           break;
         }
-        if (item == "a") {
+        //if (item == "a") {
 
-          boost::fibers::fiber t([this]() {
+        //  boost::fibers::fiber t([this]() {
 
-            std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item Name: ";
-            std::string newName = m_q->Pop();
-            std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item value: ";
-            std::string newValue = m_q->Pop();
-            std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item: NAME = " << newName << " VALUE = " << newValue << std::endl;
+        //    std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item Name: ";
+        //    std::string newName = m_q->Pop();
+        //    std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item value: ";
+        //    std::string newValue = m_q->Pop();
+        //    std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item: NAME = " << newName << " VALUE = " << newValue << std::endl;
 
-            boost::fibers::fiber([this, newName, newValue]() {m_store.AddItem(newName, newValue); }).detach();
-            });
+        //    boost::fibers::fiber([this, newName, newValue]() {m_store.AddItem(newName, newValue); }).detach();
+        //    });
 
-          t.join();
-        }
-        else if (item == "p") {
-          boost::fibers::fiber t([this]() {
+        //  t.join();
+        //}
+        //else 
+        //  if (item == "p") {
+        //  boost::fibers::fiber t([this]() {
 
-            std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item:  Enter Item Name: ";
-            std::string itemName = m_q->Pop();
-            std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item: NAME = " << itemName << std::endl;
+        //    std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item:  Enter Item Name: ";
+        //    std::string itemName = m_q->Pop();
+        //    std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item: NAME = " << itemName << std::endl;
 
-            boost::fibers::fiber([this, itemName]() {
-              auto returnedItem = m_store.GetItem(itemName);
-              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Got Item: NAME = " << itemName << " VALUE = " << (*returnedItem).second << std::endl;
-              }).detach();
-
-            //tp.doJob([this, itemName]() {
-            //  auto returnedItem = m_store.GetItem(itemName);
-            //  std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Got Item: NAME = " << itemName << " VALUE = " << (*returnedItem).second << std::endl;
-            //  });
+        //    boost::fibers::fiber([this, itemName]() {
+        //      auto returnedItem = m_store.GetItem(itemName);
+        //      std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Got Item: NAME = " << itemName << " VALUE = " << returnedItem << std::endl;
+        //      }).detach();
 
 
-            });
+        //    });
 
-          t.join();
-        }
-        else if (item == "r") {
-          boost::fibers::fiber t([this]() {
+        //  t.join();
+        //}
+        //else 
+        //    if (item == "r") {
+        //  boost::fibers::fiber t([this]() {
 
-            std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item:  Enter Item Name: ";
-            std::string itemName = m_q->Pop();
-            std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item: NAME = " << itemName << std::endl;
+        //    std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item:  Enter Item Name: ";
+        //    std::string itemName = m_q->Pop();
+        //    std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item: NAME = " << itemName << std::endl;
 
-            boost::fibers::fiber([this, itemName]() {
-              m_store.RemoveItem(itemName);
-              std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": ThreadedQueueProcessor:  Removed Item: NAME = " << itemName << std::endl;
-              }).detach();
+        //    boost::fibers::fiber([this, itemName]() {
+        //      m_store.RemoveItem(itemName);
+        //      std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": ThreadedQueueProcessor:  Removed Item: NAME = " << itemName << std::endl;
+        //      }).detach();
 
-            //tp.doJob([this, itemName]() {
-            //  m_store.RemoveItem(itemName);
-            //  std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": ThreadedQueueProcessor:  Removed Item: NAME = " << itemName << std::endl;
-            //  });
+        //    }
+        //  );
 
-            });
+        //  t.join();
+        //}
+        //else
+          if (item == "am") {
 
-          t.join();
-        }
+            boost::fibers::fiber t([this]() {
 
-        else {
-          std::cout << "Read '" << item << "' from stdin, which is not a valid message" << std::endl;
-        }
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item Name: ";
+              std::string newName = m_q->Pop();
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item string value: ";
+              std::string newStringValue = m_q->Pop();
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item:  Enter Item int value: ";
+              int newIntValue = std::stoi(m_q->Pop());
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Adding Item: NAME = " << newName << " STRING_VALUE = " << newStringValue << " INT_VALUE = " << newIntValue << std::endl;
+
+              boost::fibers::fiber(
+                [this, newName, newStringValue, newIntValue]() {
+                  m_multiItemStore.AddItem(newName, newStringValue, newIntValue); 
+                }).detach();
+              });
+
+            t.join();
+
+          }
+          else 
+            if (item == "pm") {
+            boost::fibers::fiber t([this]() {
+
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item:  Enter Item Name: ";
+              std::string itemName = m_q->Pop();
+              std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Printing Item: NAME = " << itemName << std::endl;
+
+              boost::fibers::fiber([this, itemName]() {
+                auto returnedItem = m_multiItemStore.GetItem(itemName);
+                std::cout << "FiberedQueueProcessor threadId = " << std::this_thread::get_id() << ": Got Item: NAME = " << itemName << " STRING_VALUE = " << std::get<0>(returnedItem) << " INT_VALUE = " << std::get<1>(returnedItem) << std::endl;
+                }).detach();
+
+
+              });
+
+            t.join();
+
+          }
+          else 
+              if (item == "rm") {
+            boost::fibers::fiber t([this]() {
+
+              std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item:  Enter Item Name: ";
+              std::string itemName = m_q->Pop();
+              std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": Removing Item: NAME = " << itemName << std::endl;
+
+              boost::fibers::fiber([this, itemName]() {
+                m_multiItemStore.RemoveItem(itemName);
+                std::cout << "ThreadedQueueProcessor threadId = " << std::this_thread::get_id() << ": ThreadedQueueProcessor:  Removed Item: NAME = " << itemName << std::endl;
+                }).detach();
+
+              });
+
+            t.join();
+          }
+
+          else {
+            std::cout << "Read '" << item << "' from stdin, which is not a valid message" << std::endl;
+          }
 
         boost::this_fiber::yield();
       }
 
       m_store.Stop();
       itemStoreFiber.join();
+      m_multiItemStore.Stop();
+      multiStoreFiber.join();
     }
 
   };
